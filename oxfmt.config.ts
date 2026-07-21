fb28481afe809161b5b75f19cc04afb1a72f53f4
Mika Vilpas <mika.vilpas@gmail.com>
2026-07-21 16:35:24 +0300

    chore: ignore all CHANGELOG.md files (#612)

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "oxfmt"
 // oxlint-disable-next-line import/no-default-export
 export default defineConfig({
   ...packageConfig,
-  ignorePatterns: ["CHANGELOG.md"],
+  ignorePatterns: ["**/CHANGELOG.md"],
 })
