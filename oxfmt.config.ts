a2a167f228db9b395db8edb405e111a473e7e56a
Mika Vilpas <mika.vilpas@gmail.com>
2026-07-21 12:15:21 +0300

    chore: oxfmt ignores CHANGELOG.md file

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,4 +1,5 @@
 import packageConfig from "@mikavilpas/oxfmt-config" with { type: "json" }
 
+packageConfig.ignorePatterns.push("CHANGELOG.md")
 // oxlint-disable-next-line import/no-default-export
 export default packageConfig
